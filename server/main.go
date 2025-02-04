332283622dd41c6f5b80f9567a5fc57038ee1a71
Amrit Singh <amrit0403@gmail.com>
2025-02-04 08:20:09 +0530

    code

---
+++ server/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/coder/websocket"
+)
+
+func echoHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := websocket.Accept(w, r, &websocket.AcceptOptions{
+		InsecureSkipVerify: true, // Disable origin verification
+	})
+	if err != nil {
+		log.Println("Failed to accept connection:", err)
+		return
+	}
+	defer conn.Close(websocket.StatusInternalError, "Internal error")
+
+	for {
+		msgType, data, err := conn.Read(r.Context())
+		if websocket.CloseStatus(err) != -1 {
+			log.Println("Connection closed:", err)
+			return
+		}
+		if err != nil {
+			log.Println("Read error:", err)
+			return
+		}
+
+		log.Printf("Received: %s\n", data)
+
+		err = conn.Write(r.Context(), msgType, data)
+		if err != nil {
+			log.Println("Write error:", err)
+			return
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/ws", echoHandler)
+	fmt.Println("Echo server started at ws://localhost:8080/ws")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
